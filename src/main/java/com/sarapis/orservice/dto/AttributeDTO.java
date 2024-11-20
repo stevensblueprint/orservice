69a32b33041416d4ed6d32328fc0af3681aa04e4
Butter - ThinkWin <butterman423@gmail.com>
2024-11-20 17:08:01 -0500

    Implemented Attribute DTO and Service

--- src/main/java/com/sarapis/orservice/dto/AttributeDTO.java
+++ src/main/java/com/sarapis/orservice/dto/AttributeDTO.java
@@ -1,4 +1,23 @@
 package com.sarapis.orservice.dto;
 
+import com.sarapis.orservice.entity.Metadata;
+import com.sarapis.orservice.entity.TaxonomyTerm;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttributeDTO {
+    private String id;
+    private String linkId;
+    private String linkType;
+    private String linkEntity;
+    private String value;
+    private TaxonomyTerm taxonomyTerm;
+    private Metadata metadata;
+    private String label;
 }
--- src/main/java/com/sarapis/orservice/service/AttributeServiceImpl.java
+++ src/main/java/com/sarapis/orservice/service/AttributeServiceImpl.java
@@ -1,4 +1,93 @@
 package com.sarapis.orservice.service;
 
-public class AttributeServiceImpl {
+import com.sarapis.orservice.dto.AttributeDTO;
+import com.sarapis.orservice.entity.Attribute;
+import com.sarapis.orservice.repository.AttributeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AttributeServiceImpl implements AttributeService {
+
+    private final AttributeRepository attributeRepository;
+
+    private AttributeDTO mapToDTO(Attribute attribute) {
+        return new AttributeDTO(
+            attribute.getId(),
+            attribute.getLinkId(),
+            attribute.getLinkType(),
+            attribute.getLinkEntity(),
+            attribute.getValue(),
+            attribute.getTaxonomyTerm(),
+            attribute.getMetadata(),
+            attribute.getLabel()
+        );
+    }
+
+    private Attribute mapToEntity(AttributeDTO attributeDTO) {
+        return new Attribute(
+            attributeDTO.getId(),
+            attributeDTO.getLinkId(),
+            attributeDTO.getLinkType(),
+            attributeDTO.getLinkEntity(),
+            attributeDTO.getValue(),
+            attributeDTO.getTaxonomyTerm(),
+            attributeDTO.getMetadata(),
+            attributeDTO.getLabel()
+        );
+    }
+
+    @Autowired
+    public AttributeServiceImpl(AttributeRepository attributeRepository) {
+        this.attributeRepository = attributeRepository;
+    }
+
+    @Override
+    public List<AttributeDTO> getAllAttributes() {
+        return this.attributeRepository.findAll()
+                .stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AttributeDTO getAttributeById(String id) {
+        Attribute attr = this.attributeRepository.findById(Long.parseLong(id))
+                .orElseThrow(() -> new RuntimeException("Attribute not found"));
+        return this.mapToDTO(attr);
+    }
+
+    @Override
+    public AttributeDTO createAttribute(AttributeDTO attributeDTO) {
+        Attribute attr = this.mapToEntity(attributeDTO);
+        Attribute savedAttr = this.attributeRepository.save(attr);
+        return this.mapToDTO(savedAttr);
+    }
+
+    @Override
+    public AttributeDTO updateAttribute(String id, AttributeDTO attributeDTO) {
+        Attribute oldAttr = this.attributeRepository.findById(Long.parseLong(id))
+                .orElseThrow(() -> new RuntimeException("Attribute not found"));
+        oldAttr.setId(attributeDTO.getId());
+        oldAttr.setLinkId(attributeDTO.getLinkId());
+        oldAttr.setLinkType(attributeDTO.getLinkType());
+        oldAttr.setLinkEntity(attributeDTO.getLinkEntity());
+        oldAttr.setValue(attributeDTO.getValue());
+        oldAttr.setTaxonomyTerm(attributeDTO.getTaxonomyTerm());
+        oldAttr.setMetadata(attributeDTO.getMetadata());
+        oldAttr.setLabel(attributeDTO.getLabel());
+
+        Attribute upAttr = this.attributeRepository.save(oldAttr);
+        return mapToDTO(upAttr);
+    }
+
+    @Override
+    public void deleteAttribute(String id) {
+        Attribute target = this.attributeRepository.findById(Long.parseLong(id))
+                .orElseThrow(() -> new RuntimeException("Attribute not found"));
+        this.attributeRepository.delete(target);
+    }
 }
