fb9e6106eb2b7cb8a4b3a5fe0366c7210b348fba
ZhangTerrence <terrencezhang77@gmail.com>
2024-11-21 13:49:40 -0500

    fixed merge error

--- src/main/java/com/sarapis/orservice/entity/core/Organization.java
+++ src/main/java/com/sarapis/orservice/entity/core/Organization.java
@@ -1,21 +1,12 @@
 package com.sarapis.orservice.entity.core;
 
 import com.sarapis.orservice.dto.OrganizationDTO;
-import com.sarapis.orservice.entity.Url;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.sarapis.orservice.entity.*;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
+
+import java.util.List;
 
 @Entity
 @Table(name = "organization")
@@ -30,31 +21,80 @@ public class Organization {
   @UuidGenerator
   private String id;
 
+  @Column(name = "name")
   private String name;
 
   @Column(name = "alternate_name")
   private String alternateName;
 
+  @Column(name = "description")
   private String description;
 
+  @Column(name = "email")
   private String email;
 
+  @Column(name = "website")
   private String website;
 
   @OneToMany
+  @JoinColumn(name = "organization_id")
   private List<Url> additionalWebsites;
+
+  // Deprecated
+  @Column(name = "tax_status")
+  private String taxStatus;
+
+  // Deprecated
+  @Column(name = "tax_id")
+  private String taxId;
 
   @Column(name = "year_incorporated")
   private int yearIncorporated;
 
+  @Column(name = "legal_status")
   private String legalStatus;
 
+  @Column(name = "logo")
   private String logo;
 
+  @Column(name = "uri")
   private String uri;
 
   @OneToOne
+  @JoinColumn(name = "parent_organization_id")
   private Organization parentOrganization;
+
+  @OneToMany
+  @JoinColumn(name = "organization_id")
+  private List<Funding> funding;
+
+  @OneToMany
+  @JoinColumn(name = "organization_id")
+  private List<Contact> contacts;
+
+  @OneToMany
+  @JoinColumn(name = "organization_id")
+  private List<Phone> phones;
+
+  @OneToMany
+  @JoinColumn(name = "organization_id")
+  private List<Location> locations;
+
+  @OneToMany
+  @JoinColumn(name = "organization_id")
+  private List<Program> programs;
+
+  @OneToMany
+  @JoinColumn(name = "organization_id")
+  private List<OrganizationIdentifier> organizationIdentifiers;
+
+  @OneToMany
+  @JoinColumn(name = "link_id")
+  private List<Attribute> attributes;
+
+  @OneToMany
+  @JoinColumn(name = "resource_id")
+  private List<Metadata> metadata;
 
   public OrganizationDTO toDTO() {
     return OrganizationDTO.builder()
