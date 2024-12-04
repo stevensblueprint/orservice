d11b6af7b2e79165ab33ee8964c2b68d1a32799d
miguel-merlin <mmerlin@stevens.edu>
2024-12-04 15:37:05 -0500

    feat: Added import and export controller

---
+++ src/main/java/com/sarapis/orservice/controller/ExportController.java
@@ -0,0 +1,10 @@
+package com.sarapis.orservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/export")
+public class ExportController {
+
+}
