43041fc5bf745756bbcbc3d360b18f2fad70ec77
maxgreen01 <69012713+maxgreen01@users.noreply.github.com>
2024-12-29 12:33:03 -0500

    implement repository, service, and controller for Phone

---
+++ src/main/java/com/sarapis/orservice/controller/PhoneController.java
@@ -0,0 +1,54 @@
+package com.sarapis.orservice.controller;
+
+import com.sarapis.orservice.dto.PhoneDTO;
+import com.sarapis.orservice.service.PhoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/phones")
+public class PhoneController {
+    private final PhoneService phoneService;
+
+    @Autowired
+    public PhoneController(PhoneService phoneService) {
+        this.phoneService = phoneService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PhoneDTO>> getAllPhones() {
+        List<PhoneDTO> phoneDTOs = this.phoneService.getAllPhones();
+        return ResponseEntity.ok(phoneDTOs);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PhoneDTO> getPhoneById(@PathVariable String id) {
+        PhoneDTO phoneDTO = this.phoneService.getPhoneById(id);
+        return ResponseEntity.ok(phoneDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<PhoneDTO> createPhone(@RequestBody PhoneDTO phoneDTO) {
+        if (phoneDTO.getId() == null) {
+            phoneDTO.setId(UUID.randomUUID().toString());
+        }
+        PhoneDTO createdPhone = this.phoneService.createPhone(phoneDTO);
+        return ResponseEntity.ok(createdPhone);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PhoneDTO> updatePhone(@PathVariable String id, @RequestBody PhoneDTO phoneDTO) {
+        PhoneDTO updatedAccessibility = this.phoneService.updatePhone(id, phoneDTO);
+        return ResponseEntity.ok(updatedAccessibility);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePhone(@PathVariable String id) {
+        this.phoneService.deletePhone(id);
+        return ResponseEntity.noContent().build();
+    }
+}
