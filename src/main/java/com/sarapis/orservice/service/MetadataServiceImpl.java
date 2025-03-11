686099bb0bbfad364095751e17d387d0f2ecf06c
Butter - ThinkWin <butterman423@gmail.com>
2025-03-11 15:49:39 -0400

    Added function to retrieve repository based on metadata type

--- src/main/java/com/sarapis/orservice/service/MetadataServiceImpl.java
+++ src/main/java/com/sarapis/orservice/service/MetadataServiceImpl.java
@@ -4,12 +4,16 @@ import com.sarapis.orservice.dto.MetadataDTO;
 import com.sarapis.orservice.dto.MetadataDTO.Response;
 import com.sarapis.orservice.mapper.MetadataMapper;
 import com.sarapis.orservice.model.Metadata;
-import com.sarapis.orservice.repository.MetadataRepository;
+import com.sarapis.orservice.repository.*;
+
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
+import com.sarapis.orservice.utils.MetadataUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +22,24 @@ import org.springframework.transaction.annotation.Transactional;
 public class MetadataServiceImpl implements MetadataService {
   private final MetadataRepository metadataRepository;
   private final MetadataMapper metadataMapper;
+
+  private final OrganizationRepository organizationRepository;
+  private final UrlRepository urlRepository;
+  private final ProgramRepository programRepository;
+  private final PhoneRepository phoneRepository;
+  private final OrganizationIdentifiersRepository organizationIdentifiersRepository;
+  private final FundingRepository fundingRepository;
+  private final ContactRepository contactRepository;
+  private final LanguageRepository languageRepository;
+  private final AccessibilityRepository accessibilityRepository;
+  private final AddressRepository addressRepository;
+  private final ScheduleRepository scheduleRepository;
+  private final LocationRepository locationRepository;
+  private final ServiceAreaRepository serviceAreaRepository;
+  private final CostOptionRepository costOptionRepository;
+  private final ServiceRepository serviceRepository;
+  private final RequiredDocumentRepository requiredDocumentRepository;
+  private final ServiceAtLocationRepository serviceAtLocationRepository;
 
   @Override
   @Transactional(readOnly = true)
@@ -33,5 +55,28 @@ public class MetadataServiceImpl implements MetadataService {
     Metadata metadata = metadataMapper.toEntity(requestDto);
     Metadata savedMetadata = metadataRepository.save(metadata);
     return metadataMapper.toResponseDTO(savedMetadata);
+  }
+
+  private JpaRepository<?, String> getRepository(String resourceType) {
+    return switch(resourceType) {
+      case MetadataUtils.ORGANIZATION_RESOURCE_TYPE -> this.organizationRepository;
+      case MetadataUtils.URL_RESOURCE_TYPE -> this.urlRepository;
+      case MetadataUtils.PROGRAM_RESOURCE_TYPE -> this.programRepository;
+      case MetadataUtils.PHONE_RESOURCE_TYPE -> this.phoneRepository;
+      case MetadataUtils.ORGANIZATION_IDENTIFIER_RESOURCE_TYPE -> this.organizationIdentifiersRepository;
+      case MetadataUtils.FUNDING_RESOURCE_TYPE -> this.fundingRepository;
+      case MetadataUtils.CONTACT_RESOURCE_TYPE -> this.contactRepository;
+      case MetadataUtils.LANGUAGE_RESOURCE_TYPE -> this.languageRepository;
+      case MetadataUtils.ACCESSIBILITY_RESOURCE_TYPE -> this.accessibilityRepository;
+      case MetadataUtils.ADDRESS_RESOURCE_TYPE -> this.addressRepository;
+      case MetadataUtils.SCHEDULE_RESOURCE_TYPE -> this.scheduleRepository;
+      case MetadataUtils.LOCATION_RESOURCE_TYPE -> this.locationRepository;
+      case MetadataUtils.SERVICE_AREA_RESOURCE_TYPE -> this.serviceAreaRepository;
+      case MetadataUtils.COST_OPTION_RESOURCE_TYPE -> this.costOptionRepository;
+      case MetadataUtils.SERVICE_RESOURCE_TYPE -> this.serviceRepository;
+      case MetadataUtils.REQUIRED_DOCUMENT_RESOURCE_TYPE -> this.requiredDocumentRepository;
+      case MetadataUtils.SERVICE_AT_LOCATION_RESOURCE_TYPE -> this.serviceAtLocationRepository;
+      default -> throw new RuntimeException("");
+    };
   }
 }
