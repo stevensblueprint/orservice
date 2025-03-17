5f85c07fb2f38e881a95686318d4dae8898c3ea2
Butter - ThinkWin <butterman423@gmail.com>
2025-03-17 02:44:18 -0400

    Added Parser Util class

--- src/main/java/com/sarapis/orservice/utils/Parser.java
+++ src/main/java/com/sarapis/orservice/utils/Parser.java
@@ -1,4 +1,21 @@
 package com.sarapis.orservice.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
 public class Parser {
+    public static Integer parseToInteger(String str) {
+        return Integer.parseInt(str);
+    }
+
+    // TODO: Implement
+    public static <T> List<T> parseToEntityList(String str, Class<T> entityType) {
+        throw new RuntimeException();
+    }
+
+    public static <T> T parseToEntity(String str, Class<T> entityType) {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.convertValue(str, entityType);
+    }
 }
