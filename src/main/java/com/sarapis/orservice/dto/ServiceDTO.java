9096c75fa7bd9e45b4181b5ecb623b1cef9b2a16
miguel-merlin <mmerlin@stevens.edu>
2025-03-20 13:06:43 -0400

    feat: added default builder in servicedto

--- src/main/java/com/sarapis/orservice/dto/ServiceDTO.java
+++ src/main/java/com/sarapis/orservice/dto/ServiceDTO.java
@@ -5,11 +5,13 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.sarapis.orservice.validator.ValidEmail;
 import com.sarapis.orservice.validator.ValidUrl;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,6 +21,7 @@ public class ServiceDTO {
   @Setter
   @NoArgsConstructor
   @AllArgsConstructor
+  @Builder
   @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
   public static class Request {
     private String id;
@@ -60,24 +63,47 @@ public class ServiceDTO {
     private String licenses;
     private String alert;
     private LocalDate lastModified;
-    private List<PhoneDTO.Request> phones;
-    private List<ScheduleDTO.Request> schedules;
-    private List<ServiceAreaDTO.Request> serviceAreas;
-    private List<LanguageDTO.Request> languages;
+
+    @Builder.Default
+    private List<PhoneDTO.Request> phones = new ArrayList<>();
+
+    @Builder.Default
+    private List<ScheduleDTO.Request> schedules = new ArrayList<>();
+
+    @Builder.Default
+    private List<ServiceAreaDTO.Request> serviceAreas = new ArrayList<>();
+
+    @Builder.Default
+    private List<LanguageDTO.Request> languages = new ArrayList<>();
+
     private OrganizationDTO.Request organization;
-    private List<FundingDTO.Request> funding;
-    private List<CostOptionDTO.Request> costOptions;
+
+    @Builder.Default
+    private List<FundingDTO.Request> funding = new ArrayList<>();
+
+    @Builder.Default
+    private List<CostOptionDTO.Request> costOptions = new ArrayList<>();
+
     private ProgramDTO.Request program;
-    private List<RequiredDocumentDTO.Request> requiredDocuments;
-    private List<ServiceAtLocationDTO.Request> serviceAtLocations;
-    private List<ContactDTO.Request> contacts;
-    private List<UrlDTO.Request> additionalUrls;
+
+    @Builder.Default
+    private List<RequiredDocumentDTO.Request> requiredDocuments = new ArrayList<>();
+
+    @Builder.Default
+    private List<ServiceAtLocationDTO.Request> serviceAtLocations = new ArrayList<>();
+
+    @Builder.Default
+    private List<ContactDTO.Request> contacts = new ArrayList<>();
+
+    @Builder.Default
+    private List<UrlDTO.Request> additionalUrls = new ArrayList<>();
   }
 
   @Getter
   @Setter
   @NoArgsConstructor
   @AllArgsConstructor
+  @Builder
   @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
   public static class Response {
     private String id;
@@ -101,19 +127,43 @@ public class ServiceDTO {
     private String licenses;
     private String alert;
     private LocalDate lastModified;
-    private List<PhoneDTO.Response> phones;
-    private List<ScheduleDTO.Response> schedules;
-    private List<ServiceAreaDTO.Response> serviceAreas;
-    private List<LanguageDTO.Response> languages;
+
+    @Builder.Default
+    private List<PhoneDTO.Response> phones = new ArrayList<>();
+
+    @Builder.Default
+    private List<ScheduleDTO.Response> schedules = new ArrayList<>();
+
+    @Builder.Default
+    private List<ServiceAreaDTO.Response> serviceAreas = new ArrayList<>();
+
+    @Builder.Default
+    private List<LanguageDTO.Response> languages = new ArrayList<>();
     private OrganizationDTO.Response organization;
-    private List<FundingDTO.Response> funding;
-    private List<CostOptionDTO.Response> costOptions;
+
+    @Builder.Default
+    private List<FundingDTO.Response> funding = new ArrayList<>();
+
+    @Builder.Default
+    private List<CostOptionDTO.Response> costOptions = new ArrayList<>();
     private ProgramDTO.Response program;
-    private List<RequiredDocumentDTO.Response> requiredDocuments;
-    private List<ServiceAtLocationDTO.Response> serviceAtLocations;
-    private List<ContactDTO.Response> contacts;
-    private List<UrlDTO.Response> additionalUrls;
-    private List<AttributeDTO.Response> attributes;
-    private List<MetadataDTO.Response> metadata;
+
+    @Builder.Default
+    private List<RequiredDocumentDTO.Response> requiredDocuments = new ArrayList<>();
+
+    @Builder.Default
+    private List<ServiceAtLocationDTO.Response> serviceAtLocations = new ArrayList<>();
+
+    @Builder.Default
+    private List<ContactDTO.Response> contacts = new ArrayList<>();
+
+    @Builder.Default
+    private List<UrlDTO.Response> additionalUrls = new ArrayList<>();
+
+    @Builder.Default
+    private List<AttributeDTO.Response> attributes = new ArrayList<>();
+
+    @Builder.Default
+    private List<MetadataDTO.Response> metadata = new ArrayList<>();
   }
 }
