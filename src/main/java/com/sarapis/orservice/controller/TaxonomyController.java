4a02d20897e1383db7f4163eff6941b70af9482d
miguel-merlin <mmerlin@stevens.edu>
2025-03-20 12:44:08 -0400

    feat: added taxonomy service

--- src/main/java/com/sarapis/orservice/controller/TaxonomyController.java
+++ src/main/java/com/sarapis/orservice/controller/TaxonomyController.java
@@ -3,21 +3,16 @@ package com.sarapis.orservice.controller;
 import com.sarapis.orservice.dto.PaginationDTO;
 import com.sarapis.orservice.dto.TaxonomyDTO;
 import com.sarapis.orservice.service.TaxonomyService;
-import com.sarapis.orservice.service.TaxonomyServiceImpl;
-import java.util.List;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,8 +46,11 @@ public class TaxonomyController {
   }
 
   @PostMapping
-  public ResponseEntity<TaxonomyDTO.Response> createTaxonomy(@RequestBody @Valid TaxonomyDTO.Request requestDto) {
-    TaxonomyDTO.Response taxonomy = taxonomyService.createTaxonomy(requestDto);
+  public ResponseEntity<TaxonomyDTO.Response> createTaxonomy(
+      @RequestBody @Valid TaxonomyDTO.Request requestDto,
+      @CookieValue(value = "updatedBy", required = false, defaultValue = "SYSTEM") String updatedBy
+      ) {
+    TaxonomyDTO.Response taxonomy = taxonomyService.createTaxonomy(requestDto, updatedBy);
     return ResponseEntity.status(HttpStatus.CREATED).body(taxonomy);
   }
 }
--- src/main/java/com/sarapis/orservice/mapper/TaxonomyMapper.java
+++ src/main/java/com/sarapis/orservice/mapper/TaxonomyMapper.java
@@ -1,12 +1,29 @@
 package com.sarapis.orservice.mapper;
 
+import static com.sarapis.orservice.utils.MetadataUtils.TAXONOMY_RESOURCE_TYPE;
+
 import com.sarapis.orservice.dto.TaxonomyDTO;
 import com.sarapis.orservice.model.Taxonomy;
+import com.sarapis.orservice.service.MetadataService;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {MetadataMapper.class, TaxonomyMapper.class})
+@Mapper(componentModel = "spring")
 public interface TaxonomyMapper {
   Taxonomy toEntity(TaxonomyDTO.Request dto);
   TaxonomyDTO.Response toResponseDTO(Taxonomy entity);
+
+  default  TaxonomyDTO.Response toResponseDTO(Taxonomy entity, MetadataService metadataService) {
+    TaxonomyDTO.Response response = toResponseDTO(entity);
+    enrichMetadata(entity, response, metadataService);
+    return response;
+  }
+
+  default void enrichMetadata(Taxonomy taxonomy, TaxonomyDTO.Response response, MetadataService metadataService) {
+    response.setMetadata(
+        metadataService.getMetadataByResourceIdAndResourceType(
+            taxonomy.getId(), TAXONOMY_RESOURCE_TYPE
+        )
+    );
+  }
 }
