26159830dff68edaf9fe15d89a777934367ff122
miguel-merlin <mmerlin@stevens.edu>
2025-03-24 23:30:22 -0400

    feat: removed dynamic origin

--- src/main/java/com/sarapis/orservice/config/SecurityConfig.java
+++ src/main/java/com/sarapis/orservice/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.sarapis.orservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,26 +10,11 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private static List<String> ALLOWED_ORIGINS;
-
-    @Value("${prod.allowed_origins:#{null}}")
-    private void setAllowedOrigins(String rawUrls) {
-        ALLOWED_ORIGINS = Optional.ofNullable(rawUrls)
-            .map(urls -> Arrays.stream(urls.split(","))
-                .map(String::trim)
-                .toList())
-            .orElse(Collections.emptyList());
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -40,18 +24,19 @@ public class SecurityConfig {
                 .requestMatchers("/").permitAll()
                 .anyRequest().authenticated())
             .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer
-                .configurationSource(corsConfigurationSource(ALLOWED_ORIGINS)))
+                .configurationSource(corsConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable)
             .build();
     }
 
-    private CorsConfigurationSource corsConfigurationSource(List<String> allowedOrigins) {
+    private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(allowedOrigins);
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowCredentials(false);
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setMaxAge(3600L);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
